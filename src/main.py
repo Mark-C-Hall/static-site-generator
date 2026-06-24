9df2697638a29722a07e34d5d09b6e736f378b1f
Mark Hall <mark.hall993@gmail.com>
2026-06-24 08:53:24 -0400

    Add TextNode and TextType classes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
-print("hello world")
+from textnode import *
+
+
+def main():
+    plain_text_node = TextNode("Hello!", TextType.PLAIN)
+    bold_text_node = TextNode("**HEY!!!**", TextType.BOLD)
+    link_text_node = TextNode(
+        "[Repo](https://github.com/Mark-C-Hall/static-site-generator)",
+        TextType.LINK,
+        "https://github.com/Mark-C-Hall/static-site-generator",
+    )
+
+    print(plain_text_node)
+    print(bold_text_node)
+    print(link_text_node)
+
+
+if __name__ == "__main__":
+    main()
